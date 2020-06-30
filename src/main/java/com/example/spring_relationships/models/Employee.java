764cf390452ea0e5a43f72ce4c2bc2e4561d92ae
idmacleod <idmacleod@gmail.com>
2020-06-30 10:48:24 +0100

    Only EmployeeController left to write

--- src/main/java/com/example/spring_relationships/models/Employee.java
+++ src/main/java/com/example/spring_relationships/models/Employee.java
@@ -1,26 +1,66 @@
 package com.example.spring_relationships.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "employee_number")
     private int employeeNumber;
 
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    private Department department;
+
+    @JsonIgnoreProperties({"employees"})
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            }
+    )
     private List<Project> projects;
 
-    public Employee(String firstName, String lastName, int employeeNumber) {
+    public Employee(String firstName, String lastName, int employeeNumber, Department department) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.employeeNumber = employeeNumber;
+        this.department = department;
         this.projects = new ArrayList<Project>();
     }
 
     public Employee() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -44,6 +84,14 @@ public class Employee {
 
     public void setEmployeeNumber(int employeeNumber) {
         this.employeeNumber = employeeNumber;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
     }
 
     public List<Project> getProjects() {
--- src/test/java/com/example/spring_relationships/SpringRelationshipsApplicationTests.java
+++ src/test/java/com/example/spring_relationships/SpringRelationshipsApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.spring_relationships;
 
+import com.example.spring_relationships.models.Department;
+import com.example.spring_relationships.models.Employee;
+import com.example.spring_relationships.models.Project;
+import com.example.spring_relationships.repositories.DepartmentRepository;
+import com.example.spring_relationships.repositories.EmployeeRepository;
+import com.example.spring_relationships.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringRelationshipsApplicationTests {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
+
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void canCreateEmployeeAndDepartment() {
+		Department development = new Department("Development");
+		departmentRepository.save(development);
+
+		Employee cameron = new Employee("Cameron", "Tait", 123, development);
+		employeeRepository.save(cameron);
+
+		Project codeclan = new Project("CodeClan", 5);
+		codeclan.addEmployee(cameron);
+		projectRepository.save(codeclan);
 	}
 
 }
