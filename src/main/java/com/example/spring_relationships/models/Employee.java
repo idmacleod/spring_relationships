f3407379da4ae62186e9f98c1769738df2c73a9d
idmacleod <idmacleod@gmail.com>
2020-06-30 09:50:12 +0100

    Added Employee class

--- src/main/java/com/example/spring_relationships/models/Employee.java
+++ src/main/java/com/example/spring_relationships/models/Employee.java
@@ -1,4 +1,60 @@
 package com.example.spring_relationships.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
+
+    private String firstName;
+
+    private String lastName;
+
+    private int employeeNumber;
+
+    private List<Project> projects;
+
+    public Employee(String firstName, String lastName, int employeeNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+        this.projects = new ArrayList<Project>();
+    }
+
+    public Employee() {}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public void addProject(Project project) {
+        this.projects.add(project);
+    }
 }
